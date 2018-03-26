592a43281c02c964e99fb5a65e85c6a62a9f951e
prittt <federico.bolelli@unimore.it>
2018-03-26 23:37:59 +0200

    Small improvements

--- libraries/Touch Screen ER-TFTM080-1/Examples/SD_Card_Bitmap/Example/Example.ino
+++ libraries/Touch Screen ER-TFTM080-1/Examples/SD_Card_Bitmap/Example/Example.ino
@@ -27,8 +27,9 @@ Works with Arduino 1.6.0 IDE
 // Standard Arduino Mega/Due shield            : <display model>,38,39,40,41
 
 // Remember to change the model parameter to suit your display module!
-UTFT myGLCD(SSD1963_800480,38,39,40,41);  //(byte model, int RS, int WR, int CS, int RST, int SER)
-   
+UTFT myGLCD(TFTM080_1_16,38,39,40,41);  //(byte model, int RS, int WR, int CS, int RST, int SER)
+
+//#define MEGA_SOFT_SPI 1
 #define SD_CS 47 
 int dispx, dispy;   
 
@@ -40,9 +41,12 @@ void setup(void) {
   pinMode(8, OUTPUT);  //backlight 
   digitalWrite(8, HIGH);//on
 // -------------------------------------------------------------
+
+  //pinMode(53, OUTPUT); // SD card CS pin
+  //digitalWrite(53, HIGH); // SD card CS pin
   
   Serial.begin(9600); 
-  
+
   myGLCD.setColor(255, 0, 0);
   myGLCD.fillRect(0, 0, 799, 479);  
   
@@ -50,35 +54,42 @@ void setup(void) {
   dispy=myGLCD.getDisplayYSize();  
    
   Serial.print("Initializing SD card..."); 
-  if (!SD.begin(SD_CS)) { 
+  while (!SD.begin(47)) { 
     Serial.println("failed!"); 
   } 
+  
   Serial.println("OK!"); 
    
-     bmpDraw("BMP_1.bmp", 0, 0);
-   delay(1000);
-   bmpDraw("BMP_2.bmp", 0, 0); 
+  bmpDraw("BMP_1.bmp", 0, 0);
   delay(1000);
-   bmpDraw("BMP_3.bmp", 0, 0); 
+  bmpDraw("BMP_2.bmp", 0, 0); 
   delay(1000);
-   bmpDraw("BMP_4.bmp", 0, 0);    
-    delay(1000);
-   bmpDraw("BMP_5.bmp", 0, 0);   
-     delay(1000);
-   bmpDraw("BMP_6.bmp", 0, 0); 
-    delay(1000);
-   bmpDraw("BMP_7.bmp", 0, 0);   
-     delay(1000);
-   bmpDraw("BMP_8.bmp", 0, 0);  
-     delay(1000);
-   bmpDraw("BMP_9.bmp", 0, 0);  
-     delay(1000);
-   bmpDraw("BMP_10.bmp", 0, 0);  
+  bmpDraw("BMP_3.bmp", 0, 0); 
+  delay(1000);
+  bmpDraw("BMP_4.bmp", 0, 0);    
+  delay(1000);
+  bmpDraw("BMP_5.bmp", 0, 0);   
+  delay(1000);
+  bmpDraw("BMP_6.bmp", 0, 0); 
+  delay(1000);
+  bmpDraw("BMP_7.bmp", 0, 0);   
+  delay(1000);
+  bmpDraw("BMP_8.bmp", 0, 0);  
+  delay(1000);
+  bmpDraw("BMP_9.bmp", 0, 0);  
+  delay(1000);
+  bmpDraw("BMP_10.bmp", 0, 0);  
   delay(1000);
 } 
    
 void loop() { 
+    Serial.print("Initializing SD card..."); 
+  if (!SD.begin(53)) { 
+    Serial.println("failed!"); 
+  }
+
 } 
+
    
 // This function opens a Windows Bitmap (BMP) file and 
 // displays it at the given coordinates.  It's sped up 
